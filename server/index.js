ab4cca007f5c2c7a240191823e35e1f2e403faa8
Talal <nweirant@gmail.com>
2019-01-04 16:18:11 -0500

    added request to handle cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,14 @@ app.use(bodyParser.json());
 //     console.log(proxy);
 //     request(proxy).pipe(res);
 // });
-
+app.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header(
+   "Access-Control-Allow-Headers",
+   "Origin, X-Requested-With, Content-Type, Accept"
+ );
+ next();
+});
 // app.use((req,res, next) => {
 //   console.log(req.url);
 //   let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
