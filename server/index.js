a4606223858297c334559d82ab8bbdbcc51e51c9
Talal <nweirant@gmail.com>
2019-01-04 15:27:14 -0500

    proxy mvp, deploying

--- server/index.js
+++ server/index.js
@@ -6,12 +6,25 @@ const request = require('request');
 app.use(express.static(__dirname + '/../public/'));
 app.use(bodyParser.json());
 
-app.use((req,res, next) => {
-  console.log(req.url);
-  let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
-  console.log(proxy);
-  request(proxy).pipe(res);
-})
+// app.use((req,res,next) => {
+//   console.log(req.url);
+//   let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
+//   console.log(proxy);
+//   request(proxy).pipe(res);
+//   next()
+// }, (req,res,next) => {
+//     console.log('ari, ',req.url);
+//     let proxy = 'http://ec2-13-59-251-226.us-east-2.compute.amazonaws.com' + req.url;
+//     console.log(proxy);
+//     request(proxy).pipe(res);
+// });
+
+// app.use((req,res, next) => {
+//   console.log(req.url);
+//   let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
+//   console.log(proxy);
+//   request(proxy).pipe(res);
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
